dd6c924b1d3da935f4b6b186946b37e6241eb24d
rishabh1S <rishabh15112001@gmail.com>
2025-09-14 03:22:36 +0530

    init: project initialise

---
+++ src/main/java/com/example/cloudnest/CloudnestApplication.java
@@ -0,0 +1,13 @@
+package com.example.cloudnest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudnestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudnestApplication.class, args);
+	}
+
+}
