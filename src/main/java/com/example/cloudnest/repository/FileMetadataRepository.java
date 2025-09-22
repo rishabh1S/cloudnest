7e5b0ba3313b8b23f2657b1a4983e4476c7538ec
rishabh1S <rishabh15112001@gmail.com>
2025-09-23 04:58:03 +0530

    feat: change primary key type from Long to UUID in FileMetadataRepository

--- src/main/java/com/example/cloudnest/repository/FileMetadataRepository.java
+++ src/main/java/com/example/cloudnest/repository/FileMetadataRepository.java
@@ -1,11 +1,13 @@
 package com.example.cloudnest.repository;
 
+import java.util.UUID;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.example.cloudnest.model.entity.FileMetadata;
 
 @Repository
-public interface FileMetadataRepository extends JpaRepository<FileMetadata, Long> {
+public interface FileMetadataRepository extends JpaRepository<FileMetadata, UUID> {
     void deleteByFilename(String filename);
 }
